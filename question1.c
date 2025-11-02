4ef254bde16a8645108eb81129922b020bd97f8c
sandip2007-ncu <sandip25csu215@ncuindia.edu>
2025-11-02 18:57:55 +0530

    Update question1.c

--- question1.c
+++ question1.c
@@ -2,7 +2,7 @@
 #include <math.h>
 
 int main() {
-    int num, originalNum, remainder, n = 0;
+    int num, originalNumber, remainder, n = 0;
     double result = 0.0;
 
     printf("Enter an integer: ");
@@ -10,17 +10,17 @@ int main() {
 
     originalNum = num;
 
-    while (originalNum != 0) {
-        originalNum /= 10;
+    while (originalNumber != 0) {
+        originalNumber /= 10;
         ++n;
     }
 
     originalNum = num;
 
-    while (originalNum != 0) {
-        remainder = originalNum % 10;
+    while (originalNumber != 0) {
+        remainder = originalNumber % 10;
         result += pow(remainder, n);
-        originalNum /= 10;
+        originalNumber /= 10;
     }
 
    
@@ -30,4 +30,5 @@ int main() {
         printf("%d is not an Armstrong number.\n", num);
 
     return 0;
+
 }
